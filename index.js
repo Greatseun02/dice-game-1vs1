7fca5c80cce9dd3f7a38c3d1d236f00afb3a2ae0
Goodnews Adewole <goodnews.adewole@example.com>
2023-05-19 10:43:24 +0100

    to change

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-var randomNumber1 = Math.floor( (Math.random()*6) + 1);
-var randomDice1 = "images/dice" + randomNumber1 + ".png"
+//this gets a random number from 1-6 and stores the number in a variable randomNumber1
+let randomNumber1 = Math.floor( (Math.random()*6) + 1);
+//this gets the path of to access the image of the random number gotten from the variable randomNUmber1
+let randomDice1 = "images/dice" + randomNumber1 + ".png"
+//using query selector I access the element with class img1 and set the image source attribute to the path we got from randomDice1
 document.querySelector(".img1").setAttribute("src", randomDice1)
 
-var randomNumber2 = Math.floor( (Math.random()*6) + 1);
-var randomDice2 = "images/dice" + randomNumber2 + ".png"
+//this gets a random number from 1-6 and stores the number in a variable randomNumber1
+let randomNumber2 = Math.floor( (Math.random()*6) + 1);
+//this gets the path of to access the image of the random number gotten from the variable randomNUmber1
+let randomDice2 = "images/dice" + randomNumber2 + ".png"
+//using query selector I access the element with class img1 and set the image source attribute to the path we got from randomDice1
 document.querySelector(".img2").setAttribute("src", randomDice2)
 
+//if randomDice2 is greater than randomDice1 it should change the element h1 to player 2 wins
 if (randomDice2 > randomDice1) {
   document.querySelector("h1").textContent = "Player 2 Wins!"
 }
-if (randomDice2 < randomDice1) {
+//else if randomDice1 is greater than randomDice2 it should change the element h1 to player 1 wins
+else if (randomDice2 < randomDice1) {
   document.querySelector("h1").textContent = "Player 1 Wins!"
 }
-if (randomDice2 === randomDice1) {
+//else if randomDice1 and randomDice 2 are equal then it is a draw
+else if (randomDice2 === randomDice1) {
   document.querySelector("h1").textContent = "Draw!"
 }
